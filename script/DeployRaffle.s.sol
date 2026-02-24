4a3ef6d9a3ff92093bdff0c6eeb04405f3cbb4f1
Maskypy <masky.phi@gmail.com>
2026-02-24 21:57:01 +0000

    feat: add DeployRaffle script

--- script/DeployRaffle.s.sol
+++ script/DeployRaffle.s.sol
@@ -6,7 +6,26 @@ import {Raffle} from "src/Raffle.sol";
 import {HelperConfig} from "script/HelperConfig.s.sol";
 
 contract DeployRaffle is Script {
-    function run() external {}
+    function run() external returns (Raffle, HelperConfig) {
+        return deployContract();
+    }
 
-    function deployContract() internal returns (Raffle, HelperConfig) {}
+    function deployContract() internal returns (Raffle, HelperConfig) {
+        HelperConfig helperConfig = new HelperConfig();
+        HelperConfig.NetworkConfig memory networkConfig = helperConfig.getConfig();
+        // Start broadcasting transactions
+        // The deployer account will be used to deploy the contract and send transactions
+        vm.startBroadcast();
+        Raffle raffle = new Raffle(
+            networkConfig.entranceFee,
+            networkConfig.interval,
+            networkConfig.vrfCoordinator,
+            networkConfig.keyHash,
+            networkConfig.subscriptionId,
+            networkConfig.callbackGasLimit
+        );
+        vm.stopBroadcast();
+
+        return (raffle, helperConfig);
+    }
 }
